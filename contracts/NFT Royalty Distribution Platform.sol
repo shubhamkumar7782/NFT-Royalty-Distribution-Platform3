22d94b35d0601f2baa6666e57bdfd441894a3fd9
shubhamkumar7782 <shubhamkumar778291@gmail.com>
2025-11-19 11:02:30 +0530

    Update NFT Royalty Distribution Platform

--- contracts/NFT Royalty Distribution Platform.sol
+++ contracts/NFT Royalty Distribution Platform.sol
@@ -1,4 +1,5 @@
 
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -132,4 +133,5 @@ contract NFTRoyaltyDistribution is ERC721Royalty, Ownable {
         return _tokenURIs[tokenId];
     }
 }
+
 
